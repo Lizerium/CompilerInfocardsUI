a951f218c1ab886f194f41a34e09b253e5b49a1e
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-10-20 11:19:00 +0300

    Auto-update headers via pre_commit.ps1 - 2025-10-20

--- CompilerInfocardsUI/CompilerInfocardsUI/Program.cs
+++ CompilerInfocardsUI/CompilerInfocardsUI/Program.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 19 октября 2025 13:36:01
- * Version: 1.0.52
+ * Last Updated: 20 октября 2025 11:18:59
+ * Version: 1.0.53
  */
 
 namespace CompilerInfocardsUI
