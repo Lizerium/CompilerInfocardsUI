3fc36248b55b46d468c2f0df8495b744962fa41a
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-04-20 16:21:27 +0300

    Auto-update headers via pre_commit.ps1 - 2026-04-20

--- CompilerInfocardsUI/CompilerInfocardsUI/Services/CommandServices.cs
+++ CompilerInfocardsUI/CompilerInfocardsUI/Services/CommandServices.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 20 апреля 2026 03:06:16
- * Version: 1.0.237
+ * Last Updated: 20 апреля 2026 16:21:26
+ * Version: 1.0.238
  */
 
 using System.ComponentModel;
