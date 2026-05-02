dba86b3a959e265e8e5183c85a326dac8e0ae523
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-05-02 19:16:18 +0300

    Auto-update headers via pre_commit.ps1 - 2026-05-02

--- CompilerInfocardsUI/CompilerInfocardsUI/AppData/Command/ExeCommandJson.cs
+++ CompilerInfocardsUI/CompilerInfocardsUI/AppData/Command/ExeCommandJson.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 01 мая 2026 06:51:56
- * Version: 1.0.250
+ * Last Updated: 02 мая 2026 19:16:16
+ * Version: 1.0.251
  */
 
 namespace CompilerInfocardsUI.AppData.Command
