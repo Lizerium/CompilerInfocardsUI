96d80c40d0d075a97ab443ef12a193b8448e7b76
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-06-02 06:51:56 +0300

    Auto-update headers via pre_commit.ps1 - 2026-06-02

--- CompilerInfocardsUI/CompilerInfocardsUI/MainForm.Designer.cs
+++ CompilerInfocardsUI/CompilerInfocardsUI/MainForm.Designer.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 01 июня 2026 14:46:37
- * Version: 1.0.281
+ * Last Updated: 02 июня 2026 06:51:55
+ * Version: 1.0.282
  */
 
 
--- NOTICE.md
+++ NOTICE.md
@@ -5,3 +5,4 @@ GitHub: https://github.com/Dvurechensky
 
 This project is part of the Lizerium ecosystem.
 This project is released under the MIT License.
+This project is released under the MIT License.
